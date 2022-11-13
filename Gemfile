3ae1bb2d9c83bd5f2a07d8c6219034b22b10ed85
nico_lrx <nicolas.leroux@sciencespo.fr>
2022-11-13 13:20:48 +0300

    added @prompt to home

--- Gemfile
+++ Gemfile
@@ -70,3 +70,5 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+gem "forest_liana", "~> 7.7"
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,19 @@
 class HomeController < ApplicationController
-
+	before_action :set_prompt, only: [:index]
 	def index
+	
+	end
+
+	private
+
+	def set_prompt
+		date = Date.today
+		@prompt = Prompt.where(publication_date: date.midnight..date.end_of_day)
+		if @prompt.present?
+			@prompt = @prompt.first
+		else
+			@prompt = Prompt.all.last
+		end
 	end
 	
 end
