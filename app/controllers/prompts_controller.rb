434f6d881047bea9319fe3bd74bb62ccaa3ca3fb
nico_lrx <nicolas.leroux@sciencespo.fr>
2022-11-13 20:00:23 +0300

    heroku

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -70,6 +70,19 @@ class PromptsController < ApplicationController
     end
 
 		def divide_prompt
-			DividePromptWorker.perform_async(@prompt.id)
+			# DividePromptWorker.perform_async(@prompt.id)
+
+			# we divide the prompt by word
+			prompt_text = @prompt..full_prompt
+			prompt_text.split(" ").each_with_index do |word, word_index|
+				word.chars.each_with_index do |char, char_index|
+					PromptLetter.create(
+						prompt_id: @prompt..id,
+						word_index: word_index,
+						letter_index: char_index,
+						content: char
+					)
+				end
+			end
 		end
 end
