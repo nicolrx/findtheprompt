99b095a7d8f396b0112d6d9a5077f18b0e4a420a
nico_lrx <nicolas.leroux@sciencespo.fr>
2022-11-13 18:17:08 +0300

    added confettis

--- app/javascript/controllers/prompt_controller.js
+++ app/javascript/controllers/prompt_controller.js
@@ -3,7 +3,12 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 	static targets = [ "letterField" ]
 
+	connect() {
+		this._selectNextElement();
+	}
+
   correct() {
+		this._launchTimer();
     var letterField = this.letterFieldTarget;
 		var correction = letterField.getAttribute("data-answer");
 		var userInput = letterField.value;
@@ -12,12 +17,28 @@ export default class extends Controller {
 			letterField.classList.remove("false");
 			letterField.classList.add("true");
 			letterField.setAttribute('disabled', '');
+			this._selectNextElement();
 			this._checkIfFinished();
 		} else {
 			letterField.classList.remove("true");
 			letterField.classList.add("false");
 		}
   }
+
+	_selectNextElement() {
+		var inputs = document.querySelectorAll('.letter-field');
+		let nextEle;
+		for (var i = 0; i < inputs.length; i++) {
+			if (!inputs[i].classList.contains("true")) {
+				nextEle = inputs[i];
+				break;
+			}
+		}
+		if(nextEle) {
+			nextEle.focus();
+			nextEle.select();
+		}
+	}
 
 	_checkIfFinished() {
 		var inputs = document.querySelectorAll('.letter-field');
@@ -29,7 +50,64 @@ export default class extends Controller {
 		});
 
 		if(check == true) {
-			console.log("VICTOIRE !");
+			let timer = document.getElementById("timer");
+			let score = timer.innerText;
+			var newTimer = document.createElement("div");
+			newTimer.setAttribute("id","timer-over");
+			newTimer.innerText = score;
+			timer.replaceWith(newTimer);
+
+			// fire some confettis
+			var count = 325;
+			var defaults = {
+				origin: { y: 0.73 }
+			};
+
+			function fire(particleRatio, opts) {
+				confetti(Object.assign({}, defaults, opts, {
+					particleCount: Math.floor(count * particleRatio)
+				}));
+			}
+
+			fire(0.25, {
+				spread: 26,
+				startVelocity: 50,
+			});
+			fire(0.2, {
+				spread: 60,
+			});
+			fire(0.35, {
+				spread: 100,
+				decay: 0.91,
+				scalar: 0.8
+			});
+			fire(0.1, {
+				spread: 120,
+				startVelocity: 20,
+				decay: 0.92,
+				scalar: 1.2
+			});
+			fire(0.1, {
+				spread: 120,
+				startVelocity: 40,
+			});
+		}
+	}
+
+	_launchTimer() {
+		let timer = document.getElementById("timer");
+		console.log(timer.innerText)
+		if(timer.innerText == "00:00") {
+			const clock = document.getElementById("timer");
+			let time = -1, intervalId;
+			function incrementTime() {
+				time++;
+				clock.textContent =
+					("0" + Math.trunc(time / 60)).slice(-2) +
+					":" + ("0" + (time % 60)).slice(-2);
+			}
+			incrementTime();
+			intervalId = setInterval(incrementTime, 1000);
 		}
 	}
 }
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "tsparticles-engine", to: "https://ga.jspm.io/npm:tsparticles-engine@2.5.2/esm/index.js"
